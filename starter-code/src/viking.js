a3af569f664ebfc79a12e70530c0b98ea33beba3
prrrcl <prrrcl@MacBook-Pro-de-prrrcl.local>
2019-06-28 17:09:01 +0200

    Started

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,8 +1,24 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+}
+Soldier.prototype.attack = function(){
+  console.log('ataqueeee')
+  return this.strength;
+  
+}
+Soldier.prototype.receiveDamage = function(damage){
+  console.log('maeshopupa ioeputa');
+  this.health =  this.health - damage;
+  
+}
 
 // Viking
-function Viking() {}
+function Viking() {
+  Soldier.call(this);
+  
+}
 
 // Saxon
 function Saxon() {}
